cc5ff4f9f5c6bddf71ce04c20d6a19f1d31ce8ba
Ana vazquez <anav4332816@gmail.com>
2026-09-08 13:07:48 -0300

    Inicialización de proyecto

---
+++ src/test/java/com/anadev/consultoriomedico/ConsultorioMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anadev.consultoriomedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
